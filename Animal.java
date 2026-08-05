43759a95f042f875775e13e19e529be203624a0d
11249a020 <11249a020@kanchiuniv.ac.in>
2026-08-05 10:16:45 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+class Animal{
+final void Sound(){
+System.out.println("Animals make Sounds.");
+}
+}
+class FinalMethodDemo{
+public static void main (String[] args){
+Animal a=new Animal();
+a.Sound();
+}
+}
